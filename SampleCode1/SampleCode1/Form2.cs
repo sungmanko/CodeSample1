03115fb802ab752b9d1fdf59eadedb8f9c8ce7e6
sungman ko <33561581+sungmanko@users.noreply.github.com>
2019-02-23 16:10:26 +0900

    New Upload

---
+++ SampleCode1/SampleCode1/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleCode1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("1");
+        }
+
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            MessageBox.Show("2");
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("4");
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("3");
+        }
+    }
+}
